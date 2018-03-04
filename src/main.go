431443bc0fc6a0792b0d4147477aad243d542f7f
Head In The Cloud <hdinthkld@noreply.github.com>
2018-03-04 21:24:57 +0000

    First draft of CVE download functions

--- src/main.go
+++ src/main.go
@@ -3,10 +3,15 @@ package main
 import (
 	"fmt"
 	"nvdcve"
+	// "strconv"
 )
 
 func main() {
 	nh := nvdcve.New()
-	err := nh.DownloadCVEs("c:\\temp", "test", nh.Settings.BaseURL+"-"+string(2018)+".xml.gz")
+	nh.Settings.BaseURL = "http://127.1.1.1/feeds/xml/cve/2.0/"
+	fmt.Println(nh.Settings.MaxCVEYear)
+
+	// Try to download all files to the specified directory and file prefix
+	err := nh.DownloadAllCVEs("c:/temp", "nvdcve-2.0")
 	fmt.Println(err)
 }
--- src/nvdcve/http.go
+++ src/nvdcve/http.go
@@ -1,12 +1,18 @@
 package nvdcve
 
 import (
+	"io"
+	"net/http"
 	"os"
+	"strings"
+	"strconv"
+	"time"
 )
 
 var httpSetup HTTPSettings
 
 type CVEDownloadError struct {
+	Url string
 }
 
 type HTTP struct {
@@ -16,23 +22,23 @@ type HTTP struct {
 // HTTPSettings is a structure will enables the various paramters used in downloading the CVEs to be modified
 type HTTPSettings struct {
 	BaseURL      string
-	baseName     string
-	suffix       string
-	minCVEYear   int
-	modifiedName string
-	updatedName  string
-	useCache     bool // Not yet implemented
+	BaseName     string
+	Suffix       string
+	MinCVEYear   int
+	MaxCVEYear	int
+	ModifiedName string
+	RecentName  string
 }
 
 // DefaultHTTPSettings provides a default set of HTTP parameters which is use to retrieve the CVE files
 var DefaultHTTPSettings = HTTPSettings{
 	BaseURL:      "https://static.nvd.nist.gov/feeds/xml/cve/2.0/",
-	baseName:     "nvdcve-2.0",
-	suffix:       ".xml.gz",
-	minCVEYear:   2002,
-	modifiedName: "modified",
-	updatedName:  "updated",
-	useCache:     false, // Not yet implemented
+	BaseName:     "nvdcve-2.0",
+	Suffix:       ".xml.gz",
+	MinCVEYear:   2002,
+	MaxCVEYear:   time.Now().Year(),
+	ModifiedName: "modified",
+	RecentName:   "recent",
 }
 
 func New() *HTTP {
@@ -45,42 +51,63 @@ func New() *HTTP {
 DownloadAllCVEs is a convenience function that will download all the CVEs ever published
 to the path specified by basename which must exist before running the function.
 */
-func (h HTTP) DownloadAllCVEs(path string, basename string) (err int) {
-	return 0
-}
+func (h HTTP) DownloadAllCVEs(path string, basename string) (err error) {
 
-/*
-DownloadCVEsLatest is a convenience function that will attempt to download only the most recently
-published CVEs. It calls the DownloadCves function for the 'latest' file whilst contains all
-updated CVEs for the last 7 days.
-*/
-func (h HTTP) DownloadCVEsLatest(path string, basename string) (err int) {
-	return 0
-}
+	var years []string // Will store all the CVE years that we need to download
 
-/*
-DownloadCVEsByYear function takes the year in which to download CVEs as an argument
-and attempts to download the XML from MITRE server using HTTP.
-*/
-func (h HTTP) DownloadCVEsByYear(path string, basename string, year int) (err int) {
-	return 0
-}
-
-/*
-DownloadCVEs is ...
-*/
-func (h HTTP) DownloadCVEs(path string, filename string, cveurl string) (err error) {
-	var e error
-
-	if _, err := os.Stat(path); os.IsNotExist(err) {
-		e = new(CVEDownloadError)
-		return e
+	// Build a slice of all CVE years that need to be downloaded
+	for y:=h.Settings.MinCVEYear; y <= h.Settings.MaxCVEYear; y++ {
+		years = append(years, strconv.Itoa(y))
 	}
 
-	return e
+	// Add the "special" files that are not year based
+	years = append(years, h.Settings.RecentName)
+	years = append(years, h.Settings.ModifiedName)
+
+	// Loops over all the defined years and downloads the file from the defined web server
+	for _,d:= range(years) {
+		fn := basename + "-" + d + h.Settings.Suffix
+		cveurl := h.Settings.BaseURL +  h.Settings.BaseName + "-" + d + ".xml.gz"
+
+		err := h.DownloadCVEs(path, fn, cveurl)
+
+		if err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+/*
+DownloadCVEs takes the give URL and attempts to download the file and store it with the path and
+filename specified.
+*/
+func (h HTTP) DownloadCVEs(path string, filename string, cveurl string) (err error) {
+	if _, err := os.Stat(path); os.IsNotExist(err) {
+		return err
+	}
+	resp, err := http.Get(cveurl)
+
+	if err != nil {
+		// TODO: Improve error routine
+		return err
+	}
+
+	defer resp.Body.Close()
+
+	f, err := os.Create(strings.Join([]string{path, filename}, "/"))
+
+	if err != nil {
+		// TODO: Improve error routine
+		return err
+	}
+
+	defer f.Close()
+
+	io.Copy(f, resp.Body)
+
+	return nil
 
 }
 
-func (e CVEDownloadError) Error() string {
-	return "Could not download CVE entries"
-}
